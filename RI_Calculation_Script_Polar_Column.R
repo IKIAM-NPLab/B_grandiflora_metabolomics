962844f244fad3f69272bcf9970aba35ef47d686
Jefferson-Fasso <jefferson.pastuna@est.ikiam.edu.ec>
2025-11-06 08:35:30 -0500

    Commit 11062025

--- RI_Calculation_Script_Polar_Column.R
+++ RI_Calculation_Script_Polar_Column.R
@@ -15,43 +15,81 @@ library("writexl")
 
 # Experimental RI for features that match with NIST libraries
 ## Loadding the retention time (RT) of each n-alkane
-alkane_rt <- c(3.4665, 5.0805, 7.2105, 9.6470, 12.1855, 14.7160, 17.1685,
-               19.5140, 21.7500, 23.8885, 25.9295, 28.1660, 31.0990, 35.0805,
-               39.1355, 41.8615, 43.9890, 45.7755, 47.3495, 48.7680, 50.0745,
-               51.2945, 52.4410, 53.6010, 54.9075)
+alkane_rt <- c(3.855, 5.395, 7.323, 9.435, 11.623, 13.823, 15.973, 18.058,
+               20.068, 22.001, 23.858, 25.645, 27.521, 29.863, 32.900, 36.808,
+               39.885, 42.153, 44.221, 46.620, 49.536)
 ## Loadding the RI of each n-alkane
-alkane_ri <- c(900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800,
-               1900, 2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800,
-               2900, 3000, 3100, 3200, 3300)
+alkane_ri <- c(1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200,
+               2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000, 3100, 3200)
 ## n-alkanes DataFrame
 alkane_data <- data.frame(rtime = alkane_rt, rindex = alkane_ri)
 
 
 ## Experimental Retention Index (RI) calculation
 ################################# MS-DIAL ######################################
-##Feature list deconvoluted with MS-DIAL
-msdial_rt <- read_excel("../B_grandiflora_metabolomics/Data/B_gradiflora_MSDIAL_Feat_list.xlsx", sheet = 1)
-##RI calculation
-msdial_ri <- indexRtime(msdial_rt$`Retention Time`, alkane_data)
-##Adding the RI to the MS-DIAL feature list
+##Feature list deconvoluted with MS-DIAL (3 to 21 min dataset)
+msdial_rt <- read_excel("../B_grandiflora_metabolomics/Data/MSDIAL_Feature_List_3to21min_Polar_Column.xlsx", sheet = 1)
+##RI calculation (3 to 21 min dataset)
+msdial_ri <- indexRtime(msdial_rt$`Average Rt(min)`, alkane_data)
+##Adding the RI to the MS-DIAL feature list (3 to 21 min dataset)
 msdial_rt$RI_using_R <- msdial_ri
-## Moving the RI column close to the RT column
-msdial_rt <- msdial_rt[,c(1:6, 13, 7:12)]
-##Exporting the MS-DIAL feature list with RI
-write_xlsx(msdial_rt, "../B_grandiflora_metabolomics/Result/B_gradiflora_MSDIAL_Feat_list_RI.xlsx")
+## Moving the RI column close to the RT column (3 to 21 min dataset)
+msdial_rt <- msdial_rt[,c(1:3, 69, 4:68)]
+##Exporting the MS-DIAL feature list with RI (3 to 21 min dataset)
+write_xlsx(msdial_rt, "../B_grandiflora_metabolomics/Result/MSDIAL_Feature_List_3to21min_Polar_Column_RI.xlsx")
 
+##Feature list deconvoluted with MS-DIAL (21 to 37 min dataset)
+msdial_rt21to37 <- read_excel("../B_grandiflora_metabolomics/Data/MSDIAL_Feature_List_21to37min_Polar_Column.xlsx", sheet = 1)
+##RI calculation (21 to 37 min dataset)
+msdial_ri21to37 <- indexRtime(msdial_rt21to37$`Average Rt(min)`, alkane_data)
+##Adding the RI to the MS-DIAL feature list (21 to 37 min dataset)
+msdial_rt21to37$RI_using_R <- msdial_ri21to37
+## Moving the RI column close to the RT column (21 to 37 min dataset)
+msdial_rt21to37 <- msdial_rt21to37[,c(1:3, 69, 4:68)]
+##Exporting the MS-DIAL feature list with RI (21 to 37 min dataset)
+write_xlsx(msdial_rt21to37, "../B_grandiflora_metabolomics/Result/MSDIAL_Feature_List_21to37min_Polar_Column_RI.xlsx")
+
+##Feature list deconvoluted with MS-DIAL (37 to 44 min dataset)
+msdial_rt37to44 <- read_excel("../B_grandiflora_metabolomics/Data/MSDIAL_Feature_List_37to44min_Polar_Column.xlsx", sheet = 1)
+##RI calculation (37 to 44 min dataset)
+msdial_ri37to44 <- indexRtime(msdial_rt37to44$`Average Rt(min)`, alkane_data)
+##Adding the RI to the MS-DIAL feature list (37 to 44 min dataset)
+msdial_rt37to44$RI_using_R <- msdial_ri37to44
+## Moving the RI column close to the RT column (37 to 44 min dataset)
+msdial_rt37to44 <- msdial_rt37to44[,c(1:3, 69, 4:68)]
+##Exporting the MS-DIAL feature list with RI (37 to 44 min dataset)
+write_xlsx(msdial_rt37to44, "../B_grandiflora_metabolomics/Result/MSDIAL_Feature_List_37to44min_Polar_Column_RI.xlsx")
 
 ################################# MZmine #######################################
-##Feature list deconvoluted with MZmine (3 to 43 min dataset)
-mzmine_rt <- read_excel("../B_grandiflora_metabolomics/Data/B_gradiflora_MZmine_Feat_list_3to43min.xlsx", sheet = 1)
-##RI calculation (3 to 43 min dataset)
+##Feature list deconvoluted with MZmine (3 to 21 min dataset)
+mzmine_rt <- read_excel("../B_grandiflora_metabolomics/Data/MZmine_Feature_List_3to21min_polar_column.xlsx", sheet = 1)
+##RI calculation (3 to 21 min dataset)
 mzmine_ri <- indexRtime(mzmine_rt$`row retention time`, alkane_data)
-##Adding the RI to the MZmine feature list (3 to 43 min dataset)
-mzmine_rt$RI <- mzmine_ri 
-## Moving the RI column close to the RT column (3 to 43 min dataset)
-mzmine_rt <- mzmine_rt[,c(1:2, 47, 3:46)]
+##Adding the RI to the MZmine feature list (3 to 21 min dataset)
+mzmine_rt$RI <- mzmine_ri
+## Moving the RI column close to the RT column (3 to 21 min dataset)
+mzmine_rt <- mzmine_rt[,c(1:3, 47, 4:46)]
 ##Exporting the MZmine feature list with RI
-write_xlsx(mzmine_rt, "../B_grandiflora_metabolomics/Result/B_gradiflora_MZmine_Feat_list_3to43min_RI.xlsx")
+write_xlsx(mzmine_rt, "../B_grandiflora_metabolomics/Result/MZmine_Feature_List_3to21min_polar_column_RI.xlsx")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ## Experimental Retention Index (RI) calculation
 ##Feature list deconvoluted with MZmine (43 to 53 min dataset)
